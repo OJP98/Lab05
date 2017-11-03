c14635776d56b49141efb6ab386e184f02a82adc
JUMPSTONIK <xtineen@gmail.com>
2017-11-03 09:00:59 -0600

    agregue la clase informe y la ortogonal

---
+++ src/Laboratorio05/Informe.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laboratorio05;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author JUMPSTONIK
+ */
+@Entity
+public class Informe implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date fecha;
+    private String cambio;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+    public Informe(){
+        fecha = new Date();
+        //cambio = ; 
+    }
+    public String hacerInforme(){
+        return this.fecha + "ha sido la cantidad de valvulas que se abrireron";
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Informe)) {
+            return false;
+        }
+        Informe other = (Informe) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Laboratorio05.Informe[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/Laboratorio05/Ortogonal.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laboratorio05;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author JUMPSTONIK
+ */
+@Entity
+public class Ortogonal extends Tanque implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Ortogonal(String Nid, double Ancho, double Largo, double Profundo) {
+        super(Nid, Ancho, Largo, Profundo);                
+        setMetros3();
+    }
+    
+    
+    @Override
+    /**
+     * Metodo que calcula la capacidad de tanque segun su forma
+     */
+    public void setMetros3(){
+        
+        capacidad = profundo*ancho*largo;
+        metros3 = capacidad;
+    }
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ortogonal)) {
+            return false;
+        }
+        Ortogonal other = (Ortogonal) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Laboratorio05.Ortogonal[ id=" + id + " ]";
+    }
+    
+}
